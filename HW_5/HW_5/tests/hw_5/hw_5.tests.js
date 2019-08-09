de6e3d66a7124bdafd40de71bc787244a935227e
Bohdan Onatsky <15bogdan17@gmail.com>
2019-08-09 18:55:55 +0300

    Test

---
+++ HW_5/HW_5/tests/hw_5/hw_5.tests.js
@@ -0,0 +1,20 @@
+// describe('HomeWork_5 tasks BinaryTree',  () => {
+//     describe('init', () => {
+//         const testData = [
+//             {
+//                 value: 10,
+//             }
+//
+//
+//         ];
+//
+//         testData.forEach(data => {
+//             const {value} = data;
+//
+//             it(`should return ${value} when a = ${value} `, () => {
+//                 assert.deepEqual(value, value);
+//             });
+//         });
+//     });
+//
+// });
